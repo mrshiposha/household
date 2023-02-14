8bafa9e94fbe500b3d90c90c2548e3334c81cf77
Daniel Shiposha <dshiposha@usetech.com>
2023-02-15 00:59:18 +0100

    fix: hearthstone

--- host/hearthstone.nix
+++ host/hearthstone.nix
@@ -1,13 +1,10 @@
-{ config, pkgs, ... }:
-let root = ./..;
-in
-{
+{ config, pkgs, ... }: let root = ./..; in {
   imports = [
-    "${root}"/bootloader.nix
-    "${root}"/system.nix
-    "${root}"/timezone.nix
-    "${root}"/shell.nix
-    "${root}"/users/mrshiposha.nix
+    "${root}/bootloader.nix"
+    "${root}/system.nix"
+    "${root}/timezone.nix"
+    "${root}/shell.nix"
+    "${root}/users/mrshiposha.nix"
   ];
 
   networking.hostname = "hearthstone";
