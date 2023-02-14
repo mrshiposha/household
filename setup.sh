052d9794b08cbd9e5f6fdf64e7fa0f914db801d2
Daniel Shiposha <dshiposha@usetech.com>
2023-02-14 01:55:44 +0100

    feat: add host to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+conf=$1
+
 scriptdir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 ln -s $scriptdir /etc/household-conf
+
+cat > /etc/configuration.nix <<- EOM
+{ config, pkgs, ... }: {
+    imports = [ /etc/household-conf/host/$conf.nix ];
+}
+EOM
