fa283c6436a1fb15cebc0cde58f8c8d46845a73a
Daniel Shiposha <dshiposha@usetech.com>
2023-02-14 02:05:04 +0100

    fix: setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ fi
 host=$1
 
 scriptdir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
-ln -s $scriptdir /etc/household-conf
+ln -nsf $scriptdir /etc/household-conf
 
 hostconf=/etc/household-conf/host/$host.nix
 
