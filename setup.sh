acf2aec816a9720d5c302c5640046bed6e5970a4
Daniel Shiposha <dshiposha@usetech.com>
2023-02-14 01:58:48 +0100

    fix: setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+if [ -z "$1" ]
+  then
+    echo "No host is supplied to set up."
+fi
+
 conf=$1
 
 scriptdir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 ln -s $scriptdir /etc/household-conf
 
-cat > /etc/configuration.nix <<- EOM
+cat > /etc/nixos/configuration.nix <<- EOM
 { config, pkgs, ... }: {
     imports = [ /etc/household-conf/host/$conf.nix ];
 }
