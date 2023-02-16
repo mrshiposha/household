47f110a62111286561a3d93e68c712db9a397b56
Daniel Shiposha <dshiposha@usetech.com>
2023-02-17 00:38:12 +0100

    fix: shell.nix

--- shell.nix
+++ shell.nix
@@ -5,13 +5,6 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    enableAutosuggestions = true;
-    plugins = [
-      {
-        file = "powerlevel10k.zsh-theme";
-        name = "powerlevel10k";
-        src = "${zsh-powerlevel10k}/share/zsh-powerlevel10k";
-      }
-    ];
+    autosuggestions.enable = true;
   };
 }
