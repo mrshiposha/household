93bc5fe6ad0d759a7ac524b673bc3970db476057
Daniel Shiposha <dshiposha@usetech.com>
2023-02-16 23:21:42 +0100

    feat: try to add powerline10k

--- shell.nix
+++ shell.nix
@@ -1,4 +1,17 @@
 { config, pkgs, ... }: with pkgs; {
   users.defaultUserShell = zsh;
   environment.shells = [ zsh ];
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    enableAutosuggestions = true;
+    plugins = [
+      {
+        file = "powerlevel10k.zsh-theme";
+        name = "powerlevel10k";
+        src = "${zsh-powerlevel10k}/share/zsh-powerlevel10k";
+      }
+    ];
+  };
 }
