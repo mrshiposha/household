5631b1ba83a0a526cfbe4cc25d09457403d2770f
Daniel Shiposha <dshiposha@usetech.com>
2023-02-18 15:36:37 +0100

    feat: use local home-manager conf

--- functions/home-manager/home.nix
+++ functions/home-manager/home.nix
@@ -1,25 +1,36 @@
-username: { pkgs, ... }: with pkgs; {
+username: { pkgs, ... }:
+let homenix = "home/${username}/.config/nixpkgs/home.nix";
+in with pkgs; {
   home.username = username;
   home.homeDirectory = "/home/${username}";
   home.stateVersion = import ../system-version.nix;
 
-  fonts.fontconfig.enable = true;
-  home.packages = [meslo-lgs-nf];
-  programs = {
-    home-manager.enable = true;
+  home.file."${homenix}" = {
+    enable = !pathExists homenix;
+    text = ''
+      home.username = username;
+      home.homeDirectory = "/home/${username}";
+      home.stateVersion = import /household-conf/functions/system-version.nix;
 
-    zsh = {
-      enable = true;
-      plugins = [
-        {
-          file = "powerlevel10k.zsh-theme";
-          name = "powerlevel10k";
-          src = "${zsh-powerlevel10k}/share/zsh-powerlevel10k";
-        }
-      ];
-      initExtra = ''
-        source ~/.p10k.zsh
-      '';
-    };
+      fonts.fontconfig.enable = true;
+      home.packages = [meslo-lgs-nf];
+      programs = {
+        home-manager.enable = true;
+
+        zsh = {
+          enable = true;
+          plugins = [
+            {
+              file = "powerlevel10k.zsh-theme";
+              name = "powerlevel10k";
+              src = "${zsh-powerlevel10k}/share/zsh-powerlevel10k";
+            }
+          ];
+          initExtra = ''
+            source ~/.p10k.zsh
+          '';
+        };
+      };
+    '';
   };
 }
--- users.nix
+++ users.nix
@@ -1,6 +1,9 @@
 { pkgs, lib, ... }: let userfiles = lib.filesystem.listFilesRecursive ./users; in {
   imports = userfiles ++ [(import ./functions/home-manager/home-manager.nix)];
 
+  home-manager.useUserPackages = true;
+  home-manager.useGlobalPkgs = true;
+
   home-manager.users = lib.listToAttrs (
     map
       (userfile: let user = lib.removeSuffix ".nix" (baseNameOf userfile); in {
