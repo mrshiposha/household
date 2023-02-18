41db0d2a6d10edfc735a256b9196de04d38f27d9
Daniel Shiposha <dshiposha@usetech.com>
2023-02-18 21:55:21 +0100

    fix: home.nix

--- common/scripts/cp-local-home-nix.sh
+++ common/scripts/cp-local-home-nix.sh
@@ -1,7 +1,7 @@
 homenix=$HOME/.config/nixpkgs/home.nix
 if [[ -L $homenix ]]; then
     location=$(readlink $homenix)
-    if [[ $location =~ ^/nix/store/.*-household-conf/common/home.nix$ ]]; then
+    if [[ $location =~ ^/nix/store/.*-home-manager-files/\.config/nixpkgs/home\.nix$ ]]; then
         cp --remove-destination $locaion $homenix
     fi
 fi
