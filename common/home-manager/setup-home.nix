9e04e457abcc003d0d0d0f32a745e52c32237c5c
Daniel Shiposha <dshiposha@usetech.com>
2023-02-18 17:12:02 +0100

    fix: setup-home.nix

--- common/home-manager/setup-home.nix
+++ common/home-manager/setup-home.nix
@@ -2,7 +2,7 @@ username: args @ { pkgs, ... }:
 let homenix = "/home/${username}/.config/nixpkgs/home.nix";
 in (import /household-conf/common/home.nix username args) // {
   home.file."home.nix" = {
-    enable = !pathExists homenix;
+    enable = !builtins.pathExists homenix;
     target = homenix;
     text = ''args @ { pkgs, ... }: with pkgs; 
     (import /household-conf/common/home.nix ${username} args) // {
