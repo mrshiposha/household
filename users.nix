fecbcb81b76eadb8cf6e4a9726bd1eabae4c25dd
Daniel Shiposha <dshiposha@usetech.com>
2023-02-18 13:09:45 +0100

    fix: users.nix

--- users.nix
+++ users.nix
@@ -3,7 +3,7 @@
 
   home-manager.users = lib.listToAttrs (
     map
-      (userfile: let user = lib.removeSuffix (lib.baseNameOf userfile) ".nix"; in {
+      (userfile: let user = lib.removeSuffix (baseNameOf userfile) ".nix"; in {
         name = user;
         value = import ../functions/home-manager/home.nix user;
       })
