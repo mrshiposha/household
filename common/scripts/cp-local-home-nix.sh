7d9e1294adba4b0cafe9236e67c7345da5a96417
Daniel Shiposha <dshiposha@usetech.com>
2023-02-18 22:18:48 +0100

    fix: cp-local-home-nix.sh

--- common/scripts/cp-local-home-nix.sh
+++ common/scripts/cp-local-home-nix.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
+
 homenix=$HOME/.config/nixpkgs/home.nix
 if [[ -L $homenix ]]; then
     location=$(readlink $homenix)
     if [[ $location =~ ^/nix/store/.*-home-manager-files/\.config/nixpkgs/home\.nix$ ]]; then
-        cp --remove-destination $locaion $homenix
+        cp --remove-destination $location $homenix
     fi
 fi
