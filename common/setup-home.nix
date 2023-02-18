442b3c058519c1af9e7cbe7b208026eb38e74730
Daniel Shiposha <dshiposha@usetech.com>
2023-02-18 22:26:58 +0100

    fix: home.nix ident

--- common/setup-home.nix
+++ common/setup-home.nix
@@ -6,15 +6,16 @@ in {
       home.file."home.nix" = {
         target = homenix;
         text = ''args @ { pkgs, lib, ... }: with pkgs; {
-          config = lib.mkMerge [
-            (import /household-conf/common/home.nix "${username}" args)
-            {
-              # NOTE: You can copy this file from the nix store to ~/.config/nixpkgs/home.nix
-              # and add your modifications here.
-              # E.g., programs.htop.enable = true;
-            }
-          ];
-        }'';
+  config = lib.mkMerge [
+    (import /household-conf/common/home.nix "${username}" args)
+    {
+      # NOTE: You can copy this file from the nix store to ~/.config/nixpkgs/home.nix
+      # and add your modifications here.
+      # E.g., programs.htop.enable = true;
+    }
+  ];
+}
+'';
       };
     }
     (import ./home.nix username args)
