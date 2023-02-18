434e53f76b1df6380cad858e5b9cadaf54b8e0f1
Daniel Shiposha <dshiposha@usetech.com>
2023-02-18 13:35:17 +0100

    feat: enable home-manager

--- functions/home-manager/home.nix
+++ functions/home-manager/home.nix
@@ -5,17 +5,21 @@ username: { pkgs, ... }: with pkgs; {
 
   fonts.fontconfig.enable = true;
   home.packages = [meslo-lgs-nf];
-  programs.zsh = {
-    enable = true;
-    plugins = [
-      {
-        file = "powerlevel10k.zsh-theme";
-        name = "powerlevel10k";
-        src = "${zsh-powerlevel10k}/share/zsh-powerlevel10k";
-      }
-    ];
-    initExtra = ''
-      source ~/.p10k.zsh
-    '';
+  programs = {
+    home-manager.enable = true;
+
+    zsh = {
+      enable = true;
+      plugins = [
+        {
+          file = "powerlevel10k.zsh-theme";
+          name = "powerlevel10k";
+          src = "${zsh-powerlevel10k}/share/zsh-powerlevel10k";
+        }
+      ];
+      initExtra = ''
+        source ~/.p10k.zsh
+      '';
+    };
   };
 }
