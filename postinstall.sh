f4d5a46a406cbb410e6024c1d760858b1baf71bd
Daniel Shiposha <dshiposha@usetech.com>
2023-02-19 16:13:07 +0100

    fix: postinstall household-owner

--- postinstall.sh
+++ postinstall.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
 set -e
+
+if [ -z "$1" ]
+  then
+    echo "No host is supplied to set up."
+    exit 1
+fi
+
+host=/household-conf/host/$1
+
 git config --system --add safe.directory /household-conf
+chown -R $(cat $host/household-owner):wheel /household-conf
