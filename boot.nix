4e4fe82f81feb4ad3926b5d4946b9e5b9e9036c7
Daniel Shiposha <dshiposha@usetech.com>
2023-02-19 02:26:45 +0100

    fix: bootloader resolution

--- boot.nix
+++ boot.nix
@@ -11,7 +11,7 @@
         version = 2;
         efiSupport = true;
         device = "nodev";
-        gxfmodeEfi = resolution;
+        gfxmodeEfi = resolution;
       };
     };
   } // (if silentBoot then {
