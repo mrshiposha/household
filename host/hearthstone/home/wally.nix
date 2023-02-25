d4e9502f071874f5e962232dcc77e2fa1e5ff226
Daniel Shiposha <dshiposha@usetech.com>
2023-02-25 23:02:19 +0100

    fix: remove common-home

--- host/hearthstone/home/wally.nix
+++ host/hearthstone/home/wally.nix
@@ -1,13 +1,13 @@
 {
-  common-home,
+  common,
   username,
   stateVersion,
   ...
 }:
 { pkgs, ... }: with pkgs; {
   imports = [
-    (import "${common-home}/base.nix" username stateVersion)
-    (import "${common-home}/config.nix" common-home)
-    "${common-home}/terminal.nix"
+    (import "${common}/home/base.nix" username stateVersion)
+    (import "${common}/functions/home-file.nix" "sway" "${common}/home" ".config/sway/config")
+    "${common}/home/terminal.nix"
   ];
 }
