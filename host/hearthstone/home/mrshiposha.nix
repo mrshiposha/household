f3c89de7ec022bfe566179a24850dbed21a6b239
Daniel Shiposha <dshiposha@usetech.com>
2023-02-25 23:01:24 +0100

    fix: try fix sway cfg

--- host/hearthstone/home/mrshiposha.nix
+++ host/hearthstone/home/mrshiposha.nix
@@ -9,6 +9,6 @@
     (import "${common}/home/base.nix" username stateVersion)
     (import "${common}/home/git.nix" person "${username}@gmail.com")
     (import "${common}/functions/home-file.nix" "sway" "${common}/home" ".config/sway/config")
-    "${common-home}/terminal.nix"
+    "${common}/home/terminal.nix"
   ];
 }
