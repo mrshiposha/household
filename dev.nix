052f7fdaa7dc5c02bb47fab468db6055f6639be0
Daniel Shiposha <dshiposha@usetech.com>
2023-02-26 14:11:45 +0100

    feat: add gcc

--- dev.nix
+++ dev.nix
@@ -1,5 +1,6 @@
 { pkgs, ... }: with pkgs; {
   environment.systemPackages = [
+    gcc
     rustup
   ];
 }
--- host/hearthstone/configuration.nix
+++ host/hearthstone/configuration.nix
@@ -15,7 +15,7 @@ in {
     "${root}/timezone.nix"
     "${root}/shell.nix"
     "${root}/base-sysenv.nix"
-    "${root}/rustup.nix"
+    "${root}/dev.nix"
     (import "${root}/displaymanager.nix" resolution)
     "${root}/users.nix"
     (import "${root}/home-manager.nix" host)
