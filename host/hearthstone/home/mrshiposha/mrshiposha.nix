a74a77742dce4a1e0f1a55f28d5e500b50786a0f
Daniel Shiposha <dshiposha@usetech.com>
2023-02-26 02:14:51 +0100

    draft: wallpapers

--- host/hearthstone/home/mrshiposha/mrshiposha.nix
+++ host/hearthstone/home/mrshiposha/mrshiposha.nix
@@ -1,16 +1,15 @@
 {
   common,
-  private,
   username,
   person,
-  stateVersion
+  stateVersion,
+  ...
 }:
 { pkgs, ... }: with pkgs; {
   imports = [
     (import "${common}/home/base.nix" username stateVersion)
     (import "${common}/home/git.nix" person "${username}@gmail.com")
     (import "${common}/functions/home-file.nix" "sway-common" "${common}/home" ".config/sway/config")
-    (import "${common}/functions/home-file.nix" "sway-private" "${private}" ".config/compositor/config")
     "${common}/home/terminal.nix"
   ];
 }
--- host/hearthstone/home/wally/wally.nix
+++ host/hearthstone/home/wally/wally.nix
@@ -8,7 +8,6 @@
   imports = [
     (import "${common}/home/base.nix" username stateVersion)
     (import "${common}/functions/home-file.nix" "sway-common" "${common}/home" ".config/sway/config")
-    (import "${common}/functions/home-file.nix" "sway-private" "${private}" ".config/compositor/config")
     "${common}/home/terminal.nix"
   ];
 }
