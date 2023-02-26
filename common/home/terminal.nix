a153eeba4c576a582269a85120f4bbc577a36dc0
Daniel Shiposha <dshiposha@usetech.com>
2023-02-26 04:48:30 +0100

    feat: alacritty opacity and colors

--- common/home/terminal.nix
+++ common/home/terminal.nix
@@ -1,3 +1,12 @@
 { pkgs, ... }: with pkgs; {
-  programs.alacritty.enable = true;
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      opacity = 0.75;
+      colors = {
+        backward = "#ffa500";
+        foreground = "#a5c90f";
+      };
+    };
+  };
 }
