9176595e14e10d039bae16fef684a29396665526
Daniel Shiposha <dshiposha@usetech.com>
2023-02-26 14:52:09 +0100

    feat: add yuck vscode ext

--- common/home/vscode.nix
+++ common/home/vscode.nix
@@ -5,6 +5,13 @@
     extensions = with pkgs.vscode-extensions; [
       vscode-extensions.matklad.rust-analyzer
       vscode-extensions.bungcip.better-toml
+    ] ++ vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "yuck";
+        publisher = "eww-yuck";
+        version = "v0.0.3";
+        sha256 = "0000000000000000000000000000000000000000000000000000";
+      }
     ];
     userSettings = {
       "terminal.integrated.fontFamily" = "MesloLGS NF";
