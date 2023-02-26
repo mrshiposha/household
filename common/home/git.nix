509e3470b3b8f072a131fff26ecc8d139fefe34c
Daniel Shiposha <mrshiposha@gmail.com>
2023-02-26 16:32:41 +0100

    fix: remove libsecret

--- common/home/git.nix
+++ common/home/git.nix
@@ -4,9 +4,7 @@
 }: { pkgs, ... }: with pkgs; {
   programs.git = {
     enable = true;
-    package = gitFull;
     userName = name;
     userEmail = email;
-    extraConfig.credential.helper = "libsecret";
   };
 }
