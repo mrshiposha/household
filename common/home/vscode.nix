c6359307a8b45d003f67bb76819e0e0322e61213
Daniel Shiposha <dshiposha@usetech.com>
2023-02-26 15:05:31 +0100

    fix: parinfer

--- common/home/vscode.nix
+++ common/home/vscode.nix
@@ -17,7 +17,7 @@
         name = "vscode-parinfer";
         publisher = "shaunlebron";
         version = "0.6.2";
-        sha256 = "DITgLedaO0Ifrttu+ZXkiaVA7Ua5RXc4jXQHPYLqrcM=";
+        sha256 = "zev0oomPf9B+TaNRnp4xcmEWJBaa+IHgysbX2G0mm0A=";
       }
     ];
     userSettings = {
