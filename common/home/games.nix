c9be1255922525ffd96c1724318e33a7c7827985
Daniel Shiposha <mrshiposha@gmail.com>
2023-03-15 01:10:56 +0100

    fix: remove games, fix vscode settings

--- common/home/games.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }: with pkgs; {
-  home.packages = [
-    lutris
-  ];
-
-  nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (lib.getName pkg) ["steam"];
-}
--- common/home/vscode.nix
+++ common/home/vscode.nix
@@ -4,10 +4,17 @@
     package = vscodium;
     extensions = with pkgs.vscode-extensions; [
       bbenoist.nix
-      matklad.rust-analyzer
       bungcip.better-toml
-      shd101wyy.markdown-preview-enhanced
+      matklad.rust-analyzer
+      vscode-extensions.jock.svg
     ] ++ vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "nix-env-selector";
+        publisher = "arrterian";
+        version = "1.0.9";
+        sha256 = "sha256-TkxqWZ8X+PAonzeXQ+sI9WI+XlqUHll7YyM7N9uErk0=";
+      }
+
       {
         name = "yuck";
         publisher = "eww-yuck";
@@ -21,11 +28,19 @@
         version = "0.6.2";
         sha256 = "zev0oomPf9B+TaNRnp4xcmEWJBaa+IHgysbX2G0mm0A=";
       }
+
+      {
+        name = "markdown-preview-enhanced";
+        publisher = "shd101wyy";
+        version = "0.6.8";
+        sha256 = "sha256-9NRaHgtyiZJ0ic6h1B01MWzYhDABAl3Jm2IUPogYWr0=";
+      }
     ];
     userSettings = {
       "update.mode" = "none";
       "terminal.integrated.fontFamily" = "MesloLGS NF";
       "files.insertFinalNewline" = true;
+      "nixEnvSelector.suggestion" = true;
     };
   };
 }
