b2305f74b8c1c83a7497a33a46471882bc98a6b2
Daniel Shiposha <mrshiposha@gmail.com>
2023-03-15 01:12:20 +0100

    feat: add polkit auth agent

--- polkit.nix
+++ polkit.nix
@@ -1,3 +1,23 @@
-{
+{ pkgs, ... }: {
   security.polkit.enable = true;
+  environment.systemPackages = [
+    pkgs.polkit_gnome
+  ];
+
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      enable = true;
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "default.target" ];
+      wants = [ "default.target" ];
+      after = [ "default.target" ];
+      serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+          Restart = "on-failure";
+          RestartSec = 1;
+          TimeoutStopSec = 10;
+        };
+    };
+  };
 }
