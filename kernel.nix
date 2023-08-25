c7984cb70a47b1c46eac2dc6cefc9af3b38134b1
Daniel Shiposha <mrshiposha@gmail.com>
2023-08-25 02:51:49 +0200

    fix: disable forwarding

--- kernel.nix
+++ kernel.nix
@@ -3,4 +3,9 @@
     if config.virtualisation.virtualbox.guest.enable
     then pkgs.linuxPackages
     else pkgs.linuxPackages_6_1;
+
+  boot.kernel.sysctl = {
+    "net.ipv4.conf.all.forwarding" = false;
+    "net.ipv6.conf.all.forwarding" = false;
+  };
 }
