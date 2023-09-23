1cfa9ac67ca91d97ca752b363db01a1db17b4632
Daniel Shiposha <mrshiposha@gmail.com>
2023-09-23 14:11:30 +0200

    fix: docker

--- docker.nix
+++ docker.nix
@@ -12,4 +12,19 @@
             setSocketVariable = true;
         };
     };
+
+    # # Fix https://github.com/NixOS/nixpkgs/issues/244159
+    nixpkgs.overlays = [
+        (let
+        pinnedPkgs = import(pkgs.fetchFromGitHub {
+            owner = "NixOS";
+            repo = "nixpkgs";
+            rev = "b6bbc53029a31f788ffed9ea2d459f0bb0f0fbfc";
+            sha256 = "sha256-JVFoTY3rs1uDHbh0llRb1BcTNx26fGSLSiPmjojT+KY=";
+        }) {};
+        in
+        final: prev: {
+            docker = pinnedPkgs.docker;
+        })
+    ];
 }
