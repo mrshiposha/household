d5eda5f95c6e20f9ab07f98f242e2b3d4cd70b0e
Daniel Shiposha <mrshiposha@gmail.com>
2023-10-01 21:59:15 +0200

    fix: disable rootles in docker

--- docker.nix
+++ docker.nix
@@ -1,6 +1,31 @@
 { pkgs, ... }: {
-    virtualisation.podman = {
+    environment.systemPackages = with pkgs; [
+        docker
+        docker-compose
+    ];
+
+    virtualisation.docker = {
         enable = true;
-        dockerCompat = true;
+        storageDriver = "btrfs";
+
+        # rootless = {
+        #     enable = true;
+        #     setSocketVariable = true;
+        # };
     };
+
+    # # Fix https://github.com/NixOS/nixpkgs/issues/244159
+    nixpkgs.overlays = [
+        (let
+        pinnedPkgs = import(pkgs.fetchFromGitHub {
+            owner = "NixOS";
+            repo = "nixpkgs";
+            rev = "b6bbc53029a31f788ffed9ea2d459f0bb0f0fbfc";
+            sha256 = "sha256-JVFoTY3rs1uDHbh0llRb1BcTNx26fGSLSiPmjojT+KY=";
+        }) {};
+        in
+        final: prev: {
+            docker = pinnedPkgs.docker;
+        })
+    ];
 }
