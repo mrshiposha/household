e43b9c7514cd0405578fee3317369af0e6fe1be0
Daniel Shiposha <mrshiposha@gmail.com>
2023-10-15 17:47:09 +0200

    fix: GTK apps startup

--- compositor.nix
+++ compositor.nix
@@ -12,8 +12,9 @@ let
     executable = true;
 
     text = ''
-  systemctl --user import-environment DISPLAY WAYLAND_DISPLAY SWAYSOCK
-  dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK
+  # systemctl --user import-environment DISPLAY WAYLAND_DISPLAY SWAYSOCK
+  # dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK
+  dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
   systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
   systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
       '';
