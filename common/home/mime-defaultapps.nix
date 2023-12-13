c747a5634d0b2baf9c7042db991e0f12d3c01cdd
Daniel Shiposha <mrshiposha@gmail.com>
2023-12-13 18:25:23 +0100

    chore: mime default apps

--- common/home/mime-defaultapps.nix
+++ common/home/mime-defaultapps.nix
@@ -3,6 +3,7 @@
     enable = true;
     defaultApplications = {
         "application/x-shellscript" = lib.mkDefault ["Alacritty.desktop"];
+        "application/x-archive" = lib.mkDefault ["xarchiver.desktop"];
     };
   };
 }
