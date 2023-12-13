d734767e77148953c2315635c8a3709f69e74dd9
Daniel Shiposha <mrshiposha@gmail.com>
2023-12-13 18:27:38 +0100

    chore: update unfree pkgs

--- unfree-pkgs.nix
+++ unfree-pkgs.nix
@@ -7,6 +7,8 @@ let unfreePkgs = [
     "vscode-extension-ms-vscode-remote-remote-ssh"
     "corefonts"
     "vista-fonts"
+    "teamspeak-client"
+    "pureref-1.11.1"
 ]; in
 { lib, ... }: {
   nixpkgs.config.allowUnfreePredicate = pkg:
