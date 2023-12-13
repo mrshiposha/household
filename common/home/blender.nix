d81b964694e35ac3020450599e5b497511857d3e
Daniel Shiposha <mrshiposha@gmail.com>
2023-12-13 18:54:25 +0100

    chore: revert 23.05

--- common/home/blender.nix
+++ common/home/blender.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }: with pkgs; {
   home.packages = [
     (callPackage ../packages/blender.nix {})
+    # (
+    #   pkgs.blender.override {
+    #     hipSupport = true;
+    #   }
+    # )
   ];
 }
