4ac5ac2ae6951576a52c96039740566e4fdf18ce
Daniel Shiposha <dev@shiposha.com>
2025-01-26 17:40:17 +0000

    fix: disable workspace animation on desktop

--- modules/user/programs/desktop/compositor/default.nix
+++ modules/user/programs/desktop/compositor/default.nix
@@ -77,14 +77,14 @@ in
 					animation = let
 						workspaces = if nixosConfig.laptop.enable
 							then "workspaces, 1, 6, default"
-							else "workspaces, 1, 8, default, fade";
+							else "workspaces, 0";
 					in [
 						"windows, 1, 7, openBezier"
 						"windowsOut, 1, 7, default, popin 80%"
 						"border, 1, 10, default"
 						"borderangle, 1, 8, default"
 						"fade, 1, 7, default"
-						"${workspaces}"
+						workspaces
 					];
 				};
 
