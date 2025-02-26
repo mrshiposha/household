72d4084ebb445e792810910243f7b7f491779e52
Daniel Shiposha <dev@shiposha.com>
2025-02-26 21:44:25 +0100

    feat: add packages lib entry

--- lib/default.nix
+++ lib/default.nix
@@ -1,37 +1,35 @@
 {
-	household = {
-		modules.system = ../modules/system;
-		modules.user = ../modules/user;
-		image = path: ../images + path;
+  household = {
+    modules.system = ../modules/system;
+    modules.user = ../modules/user;
+    packages = ../packages;
+    image = path: ../images + path;
 
-		usersGid = 100;
+    usersGid = 100;
 
-		eachSystemConfig = { nixpkgs, fleet, ... }: system:
-			let pkgs = import nixpkgs { inherit system; }; in {
-				devShells.default = pkgs.mkShell {
-				packages = [ fleet.packages.${system}.default ];
-			};
-		};
+    eachSystemConfig = { nixpkgs, fleet, ... }:
+      system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default =
+          pkgs.mkShell { packages = [ fleet.packages.${system}.default ]; };
+      };
 
-		userModulesByName =
-			config: moduleName:
-			let
-				users = builtins.attrValues config.home-manager.users;
-			in
-			builtins.map (user: user.${moduleName}) (builtins.filter (user: builtins.hasAttr "${moduleName}" user) users);
+    userModulesByName = config: moduleName:
+      let users = builtins.attrValues config.home-manager.users;
+      in builtins.map (user: user.${moduleName})
+      (builtins.filter (user: builtins.hasAttr "${moduleName}" user) users);
 
-		greeterThemeFromUserTheme =
-			user:
-			let
-				theming = user.theming.gui;
-			in
-			{
-				wallpaper = theming.wallpapers.screensaver;
-				style = theming.style;
-				cursors = theming.cursors;
-				icons = theming.icons;
-			} // (if theming.fonts.defaults.enable then {
-				font.name = builtins.head theming.fonts.defaults.serif;
-			} else {});
-	};
+    greeterThemeFromUserTheme = user:
+      let theming = user.theming.gui;
+      in {
+        wallpaper = theming.wallpapers.screensaver;
+        style = theming.style;
+        cursors = theming.cursors;
+        icons = theming.icons;
+      } // (if theming.fonts.defaults.enable then {
+        font.name = builtins.head theming.fonts.defaults.serif;
+      } else
+        { });
+  };
 }
