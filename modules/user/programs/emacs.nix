3e6e4df3a09bb3f3c79ab570bce8877fbd6acb06
Daniel Shiposha <dev@shiposha.com>
2026-04-25 20:23:08 +0200

    make emacs transparent

--- modules/user/programs/emacs.nix
+++ modules/user/programs/emacs.nix
@@ -24,6 +24,15 @@ with lib;
            '("7addbdeb9a444444c3114f228808c398eff733b0bd6e3050469b46f4d88c55d6"
              default)))
 
+      (set-frame-parameter nil 'alpha-background 70)
+      (add-to-list 'default-frame-alist '(alpha-background . 70))
+
+      (defun on-after-init ()
+        (unless (display-graphic-p (selected-frame))
+          (set-face-background 'default "unspecified-bg" (selected-frame))))
+
+      (add-hook 'window-setup-hook 'on-after-init)
+
       (global-display-line-numbers-mode 1)
       (direnv-mode)
     '';
